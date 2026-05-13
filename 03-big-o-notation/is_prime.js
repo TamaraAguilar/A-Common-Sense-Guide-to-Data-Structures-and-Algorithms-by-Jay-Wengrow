157d693137ce30ab00125751f683405c52a6f578
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-05-12 20:22:07 -0400

    feat: add is_prime

---
+++ 03-big-o-notation/is_prime.js
@@ -0,0 +1,11 @@
+const is_prime = (number) => {
+  for (let i = 2; i < number; i++) {
+    if (number % i == 0) {
+      return false;
+    }
+  }
+  return true;
+};
+
+// --- Tests ---
+console.log(is_prime(7));
