ad69387bd848c58c9187582616b7c5f14aff4be3
TamaraAguilar <tamaraaguilar5@gmail.com>
2026-05-21 20:24:27 -0400

    feat: add evenNumbers

--- 11-learning-to-write-in-recursive/evenNumbers.js
+++ 11-learning-to-write-in-recursive/evenNumbers.js
@@ -1,2 +1,19 @@
 /* Use recursion to write a function that accepts an array of numbers and
 returns a new array containing just the even numbers */
+
+const evenNumbers = (arr) => {
+  // Base case
+  if (arr.length == 0) {
+    return [];
+  }
+
+  if (arr[0] % 2 == 0) {
+    return [arr[0], ...evenNumbers(arr.slice(1))];
+  } else {
+    return evenNumbers(arr.slice(1));
+  }
+};
+
+// --- Tests ---
+const arr = [1, 2, 3, 4, 5];
+console.log(evenNumbers(arr));
